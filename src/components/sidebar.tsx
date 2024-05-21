8639c946fb57435525e4416b205b74bdb8acbb96
fjordservers <128976682+fjordservers@users.noreply.github.com>
2024-05-21 16:43:09 +0400

    Update sidebar.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -126,7 +126,7 @@ export function Sidebar({
                 alt="AI"
                 width={28}
                 height={28}
-                className="dark:invert hidden 2xl:block"
+                className="idden 2xl:block"
               />
             )}
             New chat
